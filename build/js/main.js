e003033da2474abe9f97a73863b90e113a267101
Avishka <avishkakapuruge@gmail.com>
2023-08-05 17:17:07 +0530

    chapter 03

--- build/js/main.js
+++ build/js/main.js
@@ -1,13 +1,38 @@
 "use strict";
-let myName = 'David';
-let isLoading;
-let album;
-let age; //union type
-let re = /\w+/g;
-myName = 'John';
-isLoading = true;
-album = 1234;
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ['one', 'hey', 'Dave'];
+let guitars = ['Strat', 'Les Paul', 9034];
+let mixedData = ['toyota', 1998, true];
+stringArr[0] = 'Mike';
+stringArr.push('Monday');
+guitars[0] = 3478;
+guitars.unshift('Jim');
+let test = [];
+let bands = [];
+bands.push('Van Halen');
+//Tuple
+let myTuple = ['Dave', 34, true];
+let mixed = ['Bob', 34, false];
+myTuple[1] = 45;
+//Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
 };
-console.log(sum(3, '2'));
+exampleObj.prop1 = 'Bob';
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+};
+let jp = {
+    name: 'Jimmy',
+    albums: ['I', 'II', 'OU812']
+};
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.name}!`;
+};
+console.log(greetGuitarist(jp));
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,59 @@
-let myName: string = 'David'
-let isLoading: boolean;
-let album: any;
-let age: string | number; //union type
-let re: RegExp = /\w+/g
+let stringArr = ['one', 'hey', 'Dave']
 
-myName = 'John'
-isLoading = true
-album = 1234
+let guitars = ['Strat', 'Les Paul', 9034]
 
-const sum = (a: number,b: string) => {
-    return a + b
+let mixedData = ['toyota', 1998, true]
+
+stringArr[0] = 'Mike'
+stringArr.push('Monday')
+
+guitars[0] = 3478
+guitars.unshift('Jim')
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+//Tuple
+let myTuple:[string, number, boolean] = ['Dave', 34 , true]
+
+let mixed = ['Bob', 34, false]
+
+myTuple[1] = 45
+
+//Objects
+let myObj: object
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
 }
 
-console.log(sum(3,'2'))
+exampleObj.prop1 = 'Bob'
+
+//type or interface
+type Guitarist = {
+    name: string,
+    active?: boolean,
+    albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+}
+
+let jp: Guitarist = {
+    name: 'Jimmy',
+    albums: ['I', 'II', 'OU812']
+}
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    return `Hello ${guitarist.name}!`
+}
+
+console.log(greetGuitarist(jp))
