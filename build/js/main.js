99807c110eaca1138a31730bbca918235e53f279
Avishka <avishkakapuruge@gmail.com>
2023-08-07 23:24:35 +0530

    chapter 05

--- build/js/main.js
+++ build/js/main.js
@@ -1,52 +1,17 @@
 "use strict";
-//Functions
-//literal types
-let myName;
-let userName;
-userName = 'Amy';
-const add = (a, b) => {
-    return a + b;
+//Assertions
+//convert to more or less specific
+let a = 'hello';
+let b = a; // less specific
+let c = a; // more specific
+let d = 'world';
+let e = 2;
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
 };
-const logMsg = (message) => {
-    console.log(message);
-};
-logMsg('Hello!!');
-logMsg(add(2, 4));
-let subtract = function (c, d) {
-    return c - d;
-};
-//interface mathFunction {(a:number, b:number): number}
-let multiply = function (c, d) {
-    return c * d;
-};
-logMsg(multiply(2, 2));
-//optional parameters
-const addAll = (a, b, c) => {
-    if (typeof c !== 'undefined') {
-        return a + b + c;
-    }
-    return a + b;
-};
-//default param value
-const sumAll = (a = 10, b, c = 2) => {
-    return a + b + c;
-};
-logMsg(addAll(2, 3, 5));
-logMsg(addAll(2, 4));
-logMsg(sumAll(2, 4));
-logMsg(sumAll(undefined, 3));
-const createError = (error) => {
-    throw new Error(error);
-};
-//custom type guard
-const isNumber = (value) => {
-    return typeof value === 'number' ? true : false;
-};
-//use of the never type
-const numberOrString = (value) => {
-    if (typeof value === 'string')
-        return 'String';
-    if (isNumber(value))
-        return 'Number';
-    return createError('This should never happen!');
-};
+let myVal = addOrConcat(2, 3, 'concat');
+// The DOM
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,26 @@
-//Functions
+//Assertions
 
-//type Aliases
-type stringOrNumber = string | number;
-
-type stringOrNumberArray = (string | number)[];
-
-type Guitarist = {
-    name: string,
-    active: boolean,
-    albums: (string | number)[];
-};
-
-type userId = stringOrNumber;
-
-//literal types
-let myName: 'Dave';
-
-let userName: 'Dave' | 'John' | 'Amy';
-userName = 'Amy';
+type One = string
+type Two = string | number
+type Three = 'hello'
 
 
-const add = (a: number, b: number): number => {
-    return a + b;
-};
+//convert to more or less specific
+let a: One = 'hello'
+let b = a as Two // less specific
+let c = a as Three // more specific
 
-const logMsg = (message: any): void => {
-    console.log(message);
-};
+let d = <One>'world'
+let e = <string | number>2
 
-logMsg('Hello!!');
-logMsg(add(2, 4));
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number |
+string => {
+    if (c === 'add') return a + b
+    return '' + a + b
+}
 
+let myVal: string = addOrConcat(2,3,'concat') as string
 
-let subtract = function (c: number, d: number): number {
-    return c - d;
-};
-
-type mathFunction = (a: number, b: number) => number;
-//interface mathFunction {(a:number, b:number): number}
-
-let multiply: mathFunction = function (c, d) {
-    return c * d;
-};
-
-logMsg(multiply(2, 2));
-
-//optional parameters
-const addAll = (a: number, b: number, c?: number): number => {
-    if (typeof c !== 'undefined') {
-        return a + b + c;
-    }
-    return a + b;
-};
-
-//default param value
-const sumAll = (a: number = 10, b: number, c: number = 2): number => {
-    return a + b + c;
-};
-
-logMsg(addAll(2, 3, 5));
-logMsg(addAll(2, 4));
-logMsg(sumAll(2, 4));
-logMsg(sumAll(undefined, 3));
-
-
-const createError = (error: string) => {
-    throw new Error(error);
-};
-
-//custom type guard
-const isNumber = (value: any): boolean => {
-    return typeof value === 'number' ? true : false;
-};
-
-//use of the never type
-const numberOrString = (value: number | string): string => {
-    if (typeof value === 'string') return 'String';
-    if (isNumber(value)) return 'Number';
-    return createError('This should never happen!');
-};
+// The DOM
+const img = document.querySelector('img')
+const myImg = document.getElementById('#img')
