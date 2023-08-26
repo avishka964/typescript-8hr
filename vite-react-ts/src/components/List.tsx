121b12b7a8aaad4b9aaca8c32421d2ccce41b10d
Avishka <avishkakapuruge@gmail.com>
2023-08-26 18:46:42 +0530

    chapter-12

---
+++ vite-react-ts/src/components/List.tsx
@@ -0,0 +1,23 @@
+//Generic type component
+
+import { ReactNode } from "react";
+
+
+interface ListProps<T> {
+    items: T[],
+    render: (item: T) => ReactNode
+}
+
+const List = <T,>({items, render}: ListProps<T>) => {
+    return (
+        <ul>
+            {items.map((item, i) => (
+                <li key={i}>
+                    {render(item)}
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default List;
